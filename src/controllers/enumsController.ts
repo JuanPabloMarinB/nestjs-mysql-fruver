043607486d295f67686cdbc09475698b56b24b10
Lihuen Lorenzo <lihuenlorenzo@gmail.com>
2023-07-01 15:38:44 -0300

    agrego enum de categoria con sus modificaciones

--- src/controllers/enumsController.ts
+++ src/controllers/enumsController.ts
@@ -1,10 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { Medida } from '../enums/Medida';
+import { CategoriaEnum } from '../enums/CategoriaEnum';
 
 @Controller('enums')
 export class EnumController {
   @Get('medida')
   getMedidaEnum() {
     return Object.values(Medida);
+  }
+
+  @Get('Categoria')
+  getCategoriaEnum() {
+    return Object.values(CategoriaEnum);
   }
 }
--- src/entities/Categoria.entity.ts
+++ src/entities/Categoria.entity.ts
@@ -1,13 +1,14 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
 import { Producto } from './Producto.entity';
+import { CategoriaEnum } from '../enums/CategoriaEnum';
 
 @Entity()
 export class Categoria {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
-  nombreCategoria: string;
+  @Column({ type: 'enum', enum: CategoriaEnum, default: CategoriaEnum.HORTALIZAS })
+  nombreCategoria: Categoria;
 
   @OneToMany(() => Producto, producto => producto.categoria)
   productos: Producto[];
