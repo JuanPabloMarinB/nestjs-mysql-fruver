49892883379db0d40d2eb5f76fb3f448db222410
JuanPabloMarinB <youtuzonhd@gmail.com>
2023-06-30 20:07:41 -0400

    arreglando import app.module.ts  1.2

--- src/modules/typeorm.config.ts
+++ src/modules/typeorm.config.ts
@@ -3,11 +3,14 @@ import { Producto } from 'src/entities/Producto.entity';
 
 const config: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: 'root',
-  database: 'inventario',
+  host: process.env.DB_HOST || 'containers-us-west-192.railway.app',
+  port: Number.parseInt(process.env.DB_PORT) || 7362,
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'gFFdMTGQfi9q9JG0uinY',
+  database: process.env.DB_NAME || 'railway',
+  ssl: {
+    rejectUnauthorized: false,
+  },
   entities: ['dist/**/*.entity{.ts,.js}'],
   synchronize: true,
 };
