28a88342e51357c9296ee2fe869947eb6ae8d239
JuanPabloMarinB <youtuzonhd@gmail.com>
2023-07-03 22:32:33 -0400

    Modificando main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './modules/app.module';
-import * as express from 'express';
 
 
 
