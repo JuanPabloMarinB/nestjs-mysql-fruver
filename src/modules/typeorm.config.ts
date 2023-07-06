bc0279432f80d208b15d702f36f254f9d9f361ec
Juan Pablo Marin Buitrago <111198348+JuanPabloMarinB@users.noreply.github.com>
2023-07-06 14:18:42 -0400

    Update typeorm.config.ts

--- src/modules/typeorm.config.ts
+++ src/modules/typeorm.config.ts
@@ -2,10 +2,10 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 const config: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: 'inventario.chagjnxwnsad.us-east-2.rds.amazonaws.com',
+  host: 'fruveraws.cjrcbjueogp4.us-east-2.rds.amazonaws.com',
   port: 3306,
-  username:'circuitos_aws',
-  password: 'VuwmtUP*fRyX!Y01',
+  username:'fruver_aws',
+  password: 'j!6AojkGOqGWPz*8',
   database: 'inventario',
   ssl: {
     rejectUnauthorized: false,
