f178dc444492901dcdb62846c5b08e9de2f69535
JuanPabloMarinB <youtuzonhd@gmail.com>
2023-07-08 02:37:40 -0400

    Agregando CORS

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './modules/app.module';
 
-
-
-const cors = require('cors');
-
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.use(cors());
 
-  // Resto de la configuración de tu aplicación
+  app.enableCors({
+    origin: 'https://main.d8xrfqlavfrwp.amplifyapp.com', // Origen permitido
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
+  });
 
   await app.listen(3000);
 }
