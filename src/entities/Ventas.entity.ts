d756c993051e05ab15d362df7a7a5c9913eff604
Lihuen Lorenzo <lihuenlorenzo@gmail.com>
2023-07-09 23:48:57 -0300

    Subo cambios de VentaServicio - Revisar gananciaXventa

--- src/entities/Ventas.entity.ts
+++ src/entities/Ventas.entity.ts
@@ -4,42 +4,45 @@ import {
   Column,
   ManyToMany,
   JoinTable,
-} from 'typeorm';
-import { Producto } from './Producto.entity';
+} from 'typeorm'
+import { Producto } from './Producto.entity'
 
 @Entity()
 export class Ventas {
   @PrimaryGeneratedColumn()
-  id: number;
+  id: number
 
-  @Column({type: 'bigint', unique: true})
-  ventaFactura: number;
+  @Column({ type: 'bigint', unique: true })
+  ventaFactura: number
 
-  @Column({type: 'datetime', default: () => 'CURRENT_TIMESTAMP'})
-  fechaVenta: Date;
+  @Column({ type: 'datetime', default: () => 'CURRENT_TIMESTAMP' })
+  fechaVenta: Date
 
   @ManyToMany(() => Producto)
   @JoinTable()
-  productos: Producto[];
-  
-  /* tener en cuenta...
-  @Column({type: 'text', array: true})
-  cantidadVenta: number[];*/
-  
-  @Column()
-  totalAPagar: number;
+  productos: Producto[]
+
+  /* tener en cuenta... */
+  @Column({ type: 'text' })
+  cantidadVenta: string
 
   @Column()
-  efectivo: number;
+  totalAPagar: number
 
   @Column()
-  cambio: number;
-/*
+  efectivo: number
+
+  @Column()
+  cambio: number
+
+  @Column()
+  gananciaXventa: number //Agregado!!!
+  /*
   constructor() {
     this.productos = [];
   }*/
 
-  constructor() {
-    this.totalAPagar = 0;
+  constructor () {
+    this.totalAPagar = 0
   }
 }
--- src/entities/VentasXDia.entity.ts
+++ src/entities/VentasXDia.entity.ts
@@ -1,33 +1,26 @@
-import {
-    Entity,
-    PrimaryGeneratedColumn,
-    Column
-  } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
 
-  @Entity()
-  export class VentasXDia {
+@Entity()
+export class VentasXDia {
+  @PrimaryGeneratedColumn()
+  id: number
 
-    @PrimaryGeneratedColumn()
-    id: number;
+  @Column()
+  diaVenta: Date
 
-    @Column()
-    diaVenta: Date;
+  //Se refiere a la cantidad de ventas que se han hecho en la fecha. Esto no se ingresa desde una solicitud, es un valor calculado automáticamente.
+  @Column()
+  totalVentasRealizadas: number
 
-    //Se refiere a la cantidad de ventas que se han hecho en la fecha. Esto no se ingresa desde una solicitud, es un valor calculado automáticamente.
-    @Column()
-    totalVentasRealizadas: number;
+  //Total acumulado de las ventas por fechas. Esto no se ingresa desde una solicitud, es un valor calculado automáticamente.
+  @Column()
+  TotalAcumulado: number
 
-    //Total acumulado de las ventas por fechas. Esto no se ingresa desde una solicitud, es un valor calculado automáticamente.
-    @Column()
-    TotalAcumulado: number;
+  //Fecha de cierre de caja. Cada día se hace un cierre de caja.
+  @Column()
+  fechaCierreCaja: Date
 
-    //Fecha de cierre de caja. Cada día se hace un cierre de caja.
-    @Column()
-    fechaCierreCaja: Date;
-
-    //Las ganancias por cierre de caja (x dia). Esto no se ingresa desde una solicitud, es un valor calculado automáticamente.
-    @Column()
-    ganancia: number;
-
-    
-  }
+  //Las ganancias por cierre de caja (x dia). Esto no se ingresa desde una solicitud, es un valor calculado automáticamente.
+  @Column()
+  ganancia: number
+}
