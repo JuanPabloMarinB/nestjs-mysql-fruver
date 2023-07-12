345c394139367e48c937250776936b34c7c0bb27
JuanPabloMarinB <youtuzonhd@gmail.com>
2023-07-12 15:43:15 -0400

    Agregar origin CORS

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './modules/app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.enableCors({
-    origin: 'https://main.d8xrfqlavfrwp.amplifyapp.com', // Origen permitido
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
-    allowedHeaders: ['Content-Type', 'Authorization'], // Encabezados permitidos
-  });
+  app.use(
+    cors({
+      origin: 'https://inventario.circuitosinteligentes.com',
+    }),
+  );
 
   await app.listen(3000);
 }
--- src/modules/typeorm.config.ts
+++ src/modules/typeorm.config.ts
@@ -2,11 +2,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 const config: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: 'fruveraws.cjrcbjueogp4.us-east-2.rds.amazonaws.com',
-  port: 3306,
-  username:'fruver_aws',
-  password: 'j!6AojkGOqGWPz*8',
-  database: 'inventario',
+  host: process.env.HOST || "fruveraws.cjrcbjueogp4.us-east-2.rds.amazonaws.com",
+  port: parseInt(process.env.PORT) || 3306,
+  username:process.env.USER || "fruver_aws",
+  password: process.env.PASSWORD || "j!6AojkGOqGWPz*8",
+  database: process.env.DATABASE || "inventario",
   ssl: {
     rejectUnauthorized: false,
   },
