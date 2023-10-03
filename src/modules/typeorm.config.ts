6fb62b0e69acfb70af2901b60679566793895a89
Juan Pablo Marin Buitrago <111198348+JuanPabloMarinB@users.noreply.github.com>
2023-10-03 12:25:28 -0300

    Update typeorm.config.ts

--- src/modules/typeorm.config.ts
+++ src/modules/typeorm.config.ts
@@ -2,13 +2,14 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 const config: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: process.env.HOST || "fruveraws.cjrcbjueogp4.us-east-2.rds.amazonaws.com",
-  port: parseInt(process.env.PORT) || 3306,
-  username:process.env.USER || "fruver_aws",
-  password: process.env.PASSWORD || "j!6AojkGOqGWPz*8",
-  database: process.env.DATABASE || "inventario",
+  host: process.env.DB_HOST || 'database-fruver.cserjyebsqnn.us-east-2.rds.amazonaws.com',
+  port: Number.parseInt(process.env.DB_PORT) || 3306,
+  username: process.env.DB_USER || 'circuitos_aws',
+  password: process.env.DB_PASSWORD || 'juanito21A#',
+  database: process.env.DB_NAME || 'db-fruver',
   ssl: {
     rejectUnauthorized: false,
+
   },
   entities: ['dist/**/*.entity{.ts,.js}'],
   synchronize: true,
